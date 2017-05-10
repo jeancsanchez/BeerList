a88e6241b55304d009e02c2434b10ad044157b37
jeancarlos <jeancs.patricio@gmail.com>
2017-05-10 14:42:19 -0300

    Integrate with Butter Knife library

--- app/src/main/java/br/com/jeancarlos/beerlist/beers/MainActivity.java
+++ app/src/main/java/br/com/jeancarlos/beerlist/beers/MainActivity.java
@@ -1,11 +1,15 @@
 package br.com.jeancarlos.beerlist.beers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import br.com.jeancarlos.beerlist.R;
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recycler_view_beers)
+    RecyclerView recyclerViewBeers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
