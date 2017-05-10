a04b60fc3ba53ea10165ed3388a58e8b4cebbfcf
jeancarlos <jeancs.patricio@gmail.com>
2017-05-10 12:45:27 -0300

    Initial Splash Screen

---
+++ app/src/main/java/br/com/jeancarlos/beerlist/SplashActivity.java
@@ -0,0 +1,21 @@
+package br.com.jeancarlos.beerlist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.jeancarlos.beerlist.beers.MainActivity;
+
+/**
+ * This class represents the Splash screen
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
