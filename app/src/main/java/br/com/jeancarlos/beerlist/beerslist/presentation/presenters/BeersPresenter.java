d201a8ab51f0d7cebae59a0c1c005fa8258df15d
jeancarlos <jeancs.patricio@gmail.com>
2017-05-13 11:36:07 -0300

    Comments on BeerPresenter

--- app/src/main/java/br/com/jeancarlos/beerlist/beerslist/presentation/presenters/BeersPresenter.java
+++ app/src/main/java/br/com/jeancarlos/beerlist/beerslist/presentation/presenters/BeersPresenter.java
@@ -6,13 +6,17 @@ import javax.inject.Inject;
 
 import br.com.jeancarlos.beerlist.beerslist.domain.model.Beer;
 import br.com.jeancarlos.beerlist.beerslist.presentation.BeersListContract;
+import br.com.jeancarlos.beerlist.beerslist.presentation.ui.MainActivity;
 import br.com.jeancarlos.beerlist.data.BeersDataSource;
 import br.com.jeancarlos.beerlist.data.BeersRepository;
 
 /**
- * This class represents a presenter for
+ * This class represents a presenter for {@link MainActivity}
+ * <p>
+ * It responds to user actions and manipulates data as required by UI
+ * </p>
  *
- * @author jeancarlos
+ * @author Jean Carlos
  * @since 5/10/17
  */
 
