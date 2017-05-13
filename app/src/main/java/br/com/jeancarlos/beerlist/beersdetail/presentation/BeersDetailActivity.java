fff57c2dde8cee0c1d0d2601515e5804e73ed8f6
jeancarlos <jeancs.patricio@gmail.com>
2017-05-13 01:08:05 -0300

    Load beer details

--- app/src/main/java/br/com/jeancarlos/beerlist/beersdetail/presentation/BeersDetailActivity.java
+++ app/src/main/java/br/com/jeancarlos/beerlist/beersdetail/presentation/BeersDetailActivity.java
@@ -1,8 +1,9 @@
 package br.com.jeancarlos.beerlist.beersdetail.presentation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -14,8 +15,17 @@ import butterknife.ButterKnife;
 
 public class BeersDetailActivity extends AppCompatActivity {
 
-    @BindView(R.id.image_view_beer_detail)
+    @BindView(R.id.image_cover_detail)
     ImageView mImageViewBeerDetail;
+
+    @BindView(R.id.text_beer_title_detail)
+    TextView mTextViewTitle;
+
+    @BindView(R.id.text_beer_subtitle_detail)
+    TextView mTextViewSubTitle;
+
+    @BindView(R.id.text_view_description)
+    TextView mTextViewDescription;
 
     private Beer mBeer;
 
@@ -28,6 +38,18 @@ public class BeersDetailActivity extends AppCompatActivity {
         mBeer = getIntent().getExtras().getParcelable(BeerHelper.KEY_BEER);
     }
 
+    // This method loads the beer details
+    private void loadBeerDetails() {
+        Picasso.with(this)
+                .load(mBeer.getImageUrl())
+                .placeholder(R.drawable.icon_star)
+                .into(mImageViewBeerDetail);
+
+        mTextViewTitle.setText(mBeer.getName());
+        mTextViewSubTitle.setText(mBeer.getTagLine());
+        mTextViewDescription.setText(mBeer.getDescription());
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -38,9 +60,6 @@ public class BeersDetailActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-
-        Picasso.with(this)
-                .load(mBeer.getImageUrl())
-                .into(mImageViewBeerDetail);
+        loadBeerDetails();
     }
 }
