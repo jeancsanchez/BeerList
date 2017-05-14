3a4cecc2b8931780416653e005a346cf512a0a55
jeancarlos <jeancs.patricio@gmail.com>
2017-05-14 19:02:34 -0300

    Docs for Retrofit service

--- app/src/main/java/br/com/jeancarlos/beerlist/data/remote/RetrofitService.java
+++ app/src/main/java/br/com/jeancarlos/beerlist/data/remote/RetrofitService.java
@@ -10,6 +10,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 /**
+ * Interface that defines all endpoints and provides methods for querying on API
+ *
  * @author Jean Carlos
  * @since 5/12/17
  */
@@ -17,9 +19,23 @@ import retrofit2.http.Query;
 public interface RetrofitService {
     String ENDPOINT = "https://api.punkapi.com/v2/";
 
+    /**
+     * Returns all beers
+     *
+     * @return Callback with a list of beers
+     * @see <a href="https://punkapi.com/documentation/v2">API Documentation</a>
+     */
     @GET("beers")
     Call<List<Beer>> getBeers();
 
+    /**
+     * Returns all beers matching the supplied name (this will match partial strings as well
+     * so e.g punk will return Punk IPA), if you need to add spaces just add an underscore (_).
+     *
+     * @param query The query String
+     * @return Callback with a list of beers
+     * @see <a href="https://punkapi.com/documentation/v2">API Documentation</a>
+     */
     @GET("beers")
     Call<List<Beer>> searchBeerByName(@Query("beer_name") String query);
 
