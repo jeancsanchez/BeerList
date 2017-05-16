5e54ad48f0bc1d0af562362ca3ce51b96e43adf1
jeancarlos <jeancs.patricio@gmail.com>
2017-05-15 22:47:09 -0300

    Fix java docs on splash screen

--- app/src/main/java/br/com/jeancarlos/beerlist/SplashActivity.java
+++ app/src/main/java/br/com/jeancarlos/beerlist/SplashActivity.java
@@ -1,13 +1,14 @@
 package br.com.jeancarlos.beerlist;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import br.com.jeancarlos.beerlist.features.beerslist.presentation.ui.MainActivity;
 
 /**
- * This class represents the Splash screen
+ * This class represents the Splash screen. We just need to call for {@link MainActivity} when this
+ * get ready. This approach prevents the blank screen at application launch.
  */
 public class SplashActivity extends AppCompatActivity {
 
